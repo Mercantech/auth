d7af23247177275f26161b69af8260f42d422ca0
MAGS <120091142+MAGS-GH@users.noreply.github.com>
2026-05-20 08:54:21 +0200

    Add site.js for clipboard functionality, enhance Clients page layout and styles, and include site.js in App.razor

--- src/Auth.API/wwwroot/js/site.js
+++ src/Auth.API/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿window.mercAuth = window.mercAuth || {};
+window.mercAuth.copyText = async function (text) {
+  await navigator.clipboard.writeText(text);
+};
